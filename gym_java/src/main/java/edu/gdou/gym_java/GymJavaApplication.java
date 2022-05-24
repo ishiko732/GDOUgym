c3b757ea329dff497696461838d073e7da49b511
liuyuanfeng <liuyuanfeng1@stu.gdou.edu.cn>
2022-05-24 12:23:48 +0800

    java project

---
+++ gym_java/src/main/java/edu/gdou/gym_java/GymJavaApplication.java
@@ -0,0 +1,13 @@
+package edu.gdou.gym_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymJavaApplication.class, args);
+    }
+
+}
---
+++ gym_java/src/test/java/edu/gdou/gym_java/GymJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.gdou.gym_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
