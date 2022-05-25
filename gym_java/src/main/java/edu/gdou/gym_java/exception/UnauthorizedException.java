37cba0d459512507522d72d3cda4a855c3b4e011
LYF <62931549+GDOU-LYF@users.noreply.github.com>
2022-05-25 18:47:51 +0800

    teamwork (#7)
    * document
    
    * test
    
    * shiro
    
    * swagger
    
    * SDK version
    
    SDK version 11 -> 8
    
    Co-authored-by: Lau225 <784461153@qq.com>
    Co-authored-by: ylhy-w <1184071961@qq.com>
    Co-authored-by: ylhy-w <53281446+ylhy-w@users.noreply.github.com>
    Co-authored-by: lizeheng666 <83770027+lizeheng666@users.noreply.github.com>

---
+++ gym_java/src/main/java/edu/gdou/gym_java/exception/UnauthorizedException.java
@@ -0,0 +1,11 @@
+package edu.gdou.gym_java.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String msg) {
+        super(msg);
+    }
+
+    public UnauthorizedException() {
+        super();
+    }
+}
