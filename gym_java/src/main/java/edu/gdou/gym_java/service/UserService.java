45e719d8e37db99454542058b398700ba6035ab1
lizeheng666 <83770027+lizeheng666@users.noreply.github.com>
2022-05-26 19:04:32 +0800

    增加查询管理员功能
    - 增加查询管理员功能，可根据用户名进行查询，用户名为空或为空串时默认查询所有管理员
    - 因为要用动态sql语句所以把sql写在xml文件里面了

--- gym_java/src/main/java/edu/gdou/gym_java/service/UserService.java
+++ gym_java/src/main/java/edu/gdou/gym_java/service/UserService.java
@@ -4,6 +4,7 @@ import edu.gdou.gym_java.entity.model.User;
 import com.baomidou.mybatisplus.extension.service.IService;
 import lombok.NonNull;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -17,5 +18,5 @@ import java.util.Map;
 public interface UserService extends IService<User> {
     Boolean register(@NonNull User user);
     User getUser(@NonNull String username);
-
+    List<User> queryManagerByUsername(String username);
 }
