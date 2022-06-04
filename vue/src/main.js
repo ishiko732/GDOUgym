49eea905d18497aea51f4d3585919abc105a89f1
Lau225 <784461153@qq.com>
2022-06-04 09:27:25 +0800

    器材模块样式完成

--- vue/src/main.js
+++ vue/src/main.js
@@ -4,10 +4,11 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import axios from 'axios'
 
+import request from "../request/request";
 
-Vue.prototype.$axios = axios
+Vue.prototype.$request=request
+
 import {
   Pagination,
   Dialog,
