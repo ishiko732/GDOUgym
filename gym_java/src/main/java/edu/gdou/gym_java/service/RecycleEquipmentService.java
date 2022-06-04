8c41da5cf32cc6ee0c5d3cfa56e70bba381cfac7
lizeheng666 <83770027+lizeheng666@users.noreply.github.com>
2022-06-04 16:45:59 +0800

    器材维修部分
    * 修改了一些bug
    * 添加器材维修申报以及确认器材维修完毕两个功能。
    也增加了查询器材可使用数量功能（审核用），详见EquipmentServiceImplement的availableEquipmentCount方法

---
+++ gym_java/src/main/java/edu/gdou/gym_java/service/RecycleEquipmentService.java
@@ -0,0 +1,15 @@
+package edu.gdou.gym_java.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import edu.gdou.gym_java.entity.model.RecycleEquipment;
+
+/**
+ * <p>
+ * 服务类
+ * </p>
+ *
+ * @author lzh
+ * @since 2022-06-04
+ */
+public interface RecycleEquipmentService extends IService<RecycleEquipment> {
+}
---
+++ gym_java/src/main/java/edu/gdou/gym_java/service/serviceImpl/RecycleEquipmentServiceImpl.java
@@ -0,0 +1,19 @@
+package edu.gdou.gym_java.service.serviceImpl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import edu.gdou.gym_java.entity.model.RecycleEquipment;
+import edu.gdou.gym_java.mapper.RecycleEquipmentMapper;
+import edu.gdou.gym_java.service.RecycleEquipmentService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author lzh
+ * @since 2022-06-04
+ */
+@Service
+public class RecycleEquipmentServiceImpl extends ServiceImpl<RecycleEquipmentMapper, RecycleEquipment> implements RecycleEquipmentService {
+}
