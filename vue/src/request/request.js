a7040c8ec9e7c4b2ac9f37fb7d479f3a13f0a452
LiuHao <78584460+Lau225@users.noreply.github.com>
2022-06-07 19:47:09 +0800

    1

--- vue/src/request/request.js
+++ vue/src/request/request.js
@@ -13,10 +13,10 @@ axios.defaults.transformRequest = [function (data) {
 }]
 
 const instance=axios.create({
-    baseURL:'http://127.0.0.1:8000/',
+    baseURL:'http://1.15.187.187:8080',
     timeout:10000,           //超过10s 则超时
 })
-
+ 
 //请求拦截器
 instance.interceptors.request.use(config=>{
     let token = localStorage.getItem("token")
@@ -29,6 +29,9 @@ instance.interceptors.request.use(config=>{
 
 //响应拦截器
 instance.interceptors.response.use(res=>{
+    // console.log(res);
+    localStorage.removeItem("token")
+    localStorage.setItem("token",res.headers.authorization)
     return res.data
 })
 
---
+++ vue/src/views/site/siteMessage.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="messageContainer">
+    <template>
+      <el-table :data="msgData" border style="width: 100%">
+        <el-table-column prop="fid" label="ID" width="180">
+        </el-table-column>
+        <el-table-column prop="description" label="场地名称" width="220">
+        </el-table-column>
+        <el-table-column prop="num" label="数量" width="180">
+        </el-table-column>
+        <el-table-column prop="money" label="费用/小时" width="220">
+        </el-table-column>
+        <el-table-column label="编辑" width="180">
+          <template slot-scope="scope">
+            <el-button @clickt="edit(scope.$index, tableData)" type="text" size="small">
+              <i class="el-icon-edit"></i>
+            </el-button>
+          </template>
+        </el-table-column>
+        <el-table-column label="删除">
+          <template slot-scope="scope">
+            <el-button @clickt="del(scope.$index, tableData)" type="text" size="small">
+              <i class="el-icon-delete"></i>
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </template>
+  </div>
+</template>
+
+<script>
+import {queryFieldType} from '@/request/api'
+export default {
+  data () { 
+    return {
+      tid: 0,
+      msgData:[]
+    }
+  },
+  created () { 
+    this.tid = this.$route.query.tid
+    queryFieldType({ tid: this.tid }).then(res => {
+      this.msgData=res.data
+    })
+  },
+  methods: {
+    del () { 
+
+    },
+    edit () {
+      
+    },
+  },
+}
+</script>
+
+<style scoped lang="less">
+.messageContainer{
+  width: 80%;
+  margin: 0 auto;
+  margin-top: 3%;
+}
+</style>
